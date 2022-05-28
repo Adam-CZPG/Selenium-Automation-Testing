8576a27896e21c03c276529ad52d54923c7d429d
Akhmadjon Ortikov (Adam) <105177864+Adam-CZPG@users.noreply.github.com>
2022-05-29 00:17:59 +0200

    Initial commit

---
+++ src/test/java/Data.java
@@ -0,0 +1,7 @@
+public class Data {
+    public String getUrl = "https://unsplash.com/";
+    public String firstName = "Adamjon25";
+    public String lastName = "Honza22";
+    public String password = "adamjon4672"
+
+}
---
+++ src/test/java/regstration.java
@@ -0,0 +1,31 @@
+import com.google.common.annotations.VisibleForTesting;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.Date;
+
+public class Registration extends Date {
+
+    private By btnSignup = By.xpath("//div[@class='a6jUt'//a[@class='cLLOA p1cWU jpBZ0 EzsBC KHq0c XHI2L'");
+    private By inputFirstName = By.xpath(//div[@class='form-group]'//input[@id='user_first_name'");
+    private By inputLastName = By.xpath(//div[@class='form-group]'//input[@id='user_last_name'");
+    private By inputEmail = By.xpath(//div[@class='form-group]'//input[@id='user_email'");
+    private By inputUserName = By.xpath(//div[@class='form-group]'//input[@id='user_username'");
+    private By inputPassword = By.xpath(//div[@class='form-group]'//input[@id='user_password'");
+    private By btnJoin =By.xpath(//div[@class='form-group]'//a[@class='btn btn-default btn-block-level js-fake-join-form-submit-button'");
+
+
+@Test
+
+    public void registration(){
+    WebDriver driver = new ChromeDriver();
+    Data getData = new Data();
+
+
+
+    }
+
+
+}
